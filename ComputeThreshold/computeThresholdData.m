53efa0c7b8c71f031f6b3c9b482f1e970fe7488f
NGavish <76819920+NGavish@users.noreply.github.com>
2021-04-26 19:06:15 +0300

    Add files via upload

---
+++ ComputeThreshold/computeThresholdData.m
@@ -0,0 +1,65 @@
+function computeThresholdData(betaVac,susceptibilityFactor,recoveredprct)
+%% Parameters
+fname=['Vthreshold_betaVac',num2str(100*betaVac),'_susceptibilityFactor',num2str(susceptibilityFactor*10),'recoveredPrct_',num2str(10*recoveredprct)];
+
+% Set efficacy in preventing disease so that overall protection in 95%
+effVac=1-0.05/betaVac;nuVac=1;
+
+% Set no active infected
+infected_nv_prct=0;infected_v_prct=0;
+
+% Maximal vaccination per age group
+maxPrct=100;
+
+%% Select Country
+country="USA";
+countryData=load(join(['../countryData/',country,'_data.mat'],''));
+
+vaccineRangeVec={'above20','above10','All','above16','above12','above6'};
+R0=1;
+
+%% Define optimization problem
+problem.options = optimoptions('fmincon','MaxFunctionEvaluations',1e4,'Display','none','Algorithm','sqp');%,'Display','iter');
+problem.solver = 'fmincon';
+
+problem.A=[];problem.B=[];
+
+[uniformAllocation,riskFocusedAllocation,spreadersAllocation,upperBound,vaccinesLeftToDistibute,adultAges,ageAbove60,age40to60,age20to40,IFR,Cij,Ni,Nadult,r,v,infected0_v,infected0_nv]=prepareFinalSizeParameters(country,'All',recoveredprct,infected_nv_prct,infected_v_prct,1,betaVac,effVac,maxPrct,true);
+
+VcPrctVec=linspace(0,100*sum(Ni)./sum(Ni(3:9)),150);
+tic
+for kx=1:numel(VcPrctVec)
+    for jx=1:numel(vaccineRangeVec)
+        VcPrct=VcPrctVec(kx);
+        
+        
+        vaccineRange=vaccineRangeVec{jx};
+        
+        
+        %% Prepare data
+        [uniformAllocation,riskFocusedAllocation,spreadersAllocation,upperBound,vaccinesLeftToDistibute,adultAges,ageAbove60,age40to60,age20to40,IFR,Cij,Ni,Nadult,r,v,infected0_v,infected0_nv]=prepareFinalSizeParameters(country,vaccineRange,recoveredprct,infected_nv_prct,infected_v_prct,VcPrct,betaVac,effVac,maxPrct,true);
+        
+        % Take into account susceptibility 
+        susceptibilityProfile=ones(1,9);susceptibilityProfile(1:2)=susceptibilityFactor;
+        Cij=diag(susceptibilityProfile)*Cij;M2=diag(Ni)*Cij*diag(1./Ni);[V,d]=eig(M2);Cij =Cij/max(d(:));
+
+        Aeq=Nadult';Beq=vaccinesLeftToDistibute;
+        lb=0*upperBound;
+        ub=upperBound;
+        
+        % Start scan from R=1 to R=4
+        w=0;
+        y=fmincon(@(x) computeMaxEig(x,adultAges,IFR,1,R0,Cij,Ni,r,v,infected0_v,infected0_nv,betaVac,nuVac,effVac,1,1),uniformAllocation,problem.A,problem.B,Aeq,Beq,lb,ub,[],problem.options);
+        distribution{jx,kx}=y;
+        result=computeMaxEig(y,adultAges,IFR,0,R0,Cij,Ni,r,v,infected0_v,infected0_nv,betaVac,nuVac,effVac,1,1);
+        Threshold(jx,kx)=1./result;
+    end
+    [uniformAllocation,riskFocusedAllocation,spreadersAllocation,upperBound,vaccinesLeftToDistibute,adultAges,ageAbove60,age40to60,age20to40,IFR,Cij,Ni,Nadult,r,v,infected0_v,infected0_nv]=prepareFinalSizeParameters(country,'All',recoveredprct,infected_nv_prct,infected_v_prct,VcPrctVec(kx),betaVac,effVac,maxPrct,true);
+    VcGnrl(kx)=100*vaccinesLeftToDistibute/sum(Ni);
+    
+end
+save(['data',fname]);
+
+return
+
+
---
+++ ComputeThreshold/presentData_susceptibility.m
@@ -0,0 +1,112 @@
+function presentData_susceptibility
+close all;
+defineColors;
+tiledlayout(2,2, 'TileSpacing', 'compact','Padding','none');
+  susceptiblityTxt={'15','20'};
+for jx_idx=1:2
+  
+load(['dataVthreshold_betaVac20_susceptibilityFactor',susceptiblityTxt{jx_idx},'recoveredPrct_0']);
+jx=jx_idx;
+axVec{jx}=nexttile(jx);
+
+prct20plus=100*sum(Ni(3:9))/sum(Ni);a=min(find(VcGnrl>prct20plus));
+plot(Threshold(1,1:a),VcGnrl(1:a),'-.','Color',blue,'linewidth',1.5);hold on;
+Rcritical20=Threshold(1,a);
+prct10plus=100*sum(Ni(2:9))/sum(Ni);a=min(find(VcGnrl>prct10plus));
+plot(Threshold(2,1:a),VcGnrl(1:a),'-','Color',orange,'linewidth',1.5);
+Rcritical10=Threshold(2,a);
+plot(Threshold(3,:),VcGnrl,'--','Color',green,'linewidth',1.5);
+
+s=max(1-(1-betaVac)*VcGnrl/100-recoveredprct/100,0);
+plot(1./s,VcGnrl,':','linewidth',2)
+xlim([1 1/betaVac]);ylim([0 100]);
+
+Rthreshold=Threshold(1,end);
+
+ylabel('V_{threshold} (% of all population)')
+xlabel('R_0');
+ytickformat( 'percentage');
+legend('Eligibilty - ages 20 and older','Eligibilty - ages 10 and older','Eligibilty - all ages','Homogeneous Threshold','location','best','autoupdate','off');
+yyaxis right;plot(1./s,VcPrctVec,'k:');
+ylim(100*[0 sum(Ni)./sum(Ni(3:9))]);
+ylabel('V_{threshold} (% of adult population)')
+ax = gca;
+ax.YAxis(2).Color = 'k'; 
+
+ax1 = gca; % position of first axes
+
+grid on;
+
+ax.YAxis(2).TickValues=[0:20:120];
+ytickformat('percentage');
+
+axVec{jx+2}=nexttile(jx+2);
+R=Threshold(3,:);
+        Mat=cell2mat(distribution(3,:));
+        hndl=area(Threshold(3,:),100*(Mat.*Ni)'/sum(Ni)) ;   %title([vaccineRange,' - along w=0']);
+        xlabel('R_0');ylabel('Vaccine supply (percent of population)');ytickformat( 'percentage');
+        dataMat=100*(Mat.*Ni)'/sum(Ni);
+        a=130;
+        textAges={'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80+'};
+        currY=-2.5;
+        for ix=1:6
+            currY=currY+dataMat(a,ix);
+            if dataMat(a,ix)>sum(dataMat(a,:))/50
+                text(R(a),currY,textAges{ix},'verticalAlign','top','color','w','fontweight','bold')
+            end
+        end
+        
+        text(R(a),currY+11,textAges{7},'verticalAlign','top','color','w','fontweight','bold')
+        
+        for ix=1:9
+            hndl(ix).FaceColor=lineColors(ix,:);
+        end
+    
+grid on
+yyaxis left;xlim([0 1./betaVac]);
+ylim([ 0 100 ]);
+
+yyaxis right;%plot(1./s,VcPrctVec,'w');
+xlim([0 1./betaVac]);
+ylim(100*[0 sum(Ni)./sum(Ni(3:9))]);
+ax = gca;
+ax.YAxis(2).Color = 'k'; 
+ylabel('V_{threshold} (% of adult population)');
+ytickformat( 'percentage');
+ax.YAxis(2).TickValues=[0:20:120];
+
+
+xlim([1 1/betaVac]);
+end
+gray=0.8*[1 1 1];
+for jx_idx=1:2
+    
+load('dataVthreshold_betaVac20_susceptibilityFactor10recoveredPrct_0');hold on;
+jx=jx_idx;
+axVec{jx}=nexttile(jx);
+
+prct20plus=100*sum(Ni(3:9))/sum(Ni);a=min(find(VcGnrl>prct20plus));
+yyaxis left;plot(Threshold(1,1:a),VcGnrl(1:a),'-.','Color',gray,'linewidth',1.5);hold on;
+Rcritical20=Threshold(1,a);ylim([0 100]);
+prct10plus=100*sum(Ni(2:9))/sum(Ni);a=min(find(VcGnrl>prct10plus));
+yyaxis left;plot(Threshold(2,1:a),VcGnrl(1:a),'-','Color',gray,'linewidth',1.5);
+Rcritical10=Threshold(2,a);
+plot(Threshold(3,:),VcGnrl,'--','Color',gray,'linewidth',1.5);
+
+s=max(1-(1-betaVac)*VcGnrl/100-recoveredprct/100,0);
+
+Rthreshold=Threshold(1,end);
+end
+linkaxes([ axVec{3} axVec{4} ],'xy');
+Ages={'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80+'};
+lg = legend(axVec{4},Ages,'fontsize',10,'Orientation','horizontal');%legend(nexttile(2), [line1,line2,line3,line4]);
+ lg.Layout.Tile='south'
+
+set(gcf,'Position',[520 426 942 486]);
+title(axVec{1},'Increased children susceptibility by factor of 1.5')
+title(axVec{2},'Increased children susceptibility by factor of 2')
+text(axVec{1},0.05,0.9,'A','units','normalized','fontweight','bold','fontsize',12);
+text(axVec{2},0.05,0.9,'C','units','normalized','fontweight','bold','fontsize',12);
+text(axVec{3},0.05,0.9,'B','units','normalized','fontweight','bold','fontsize',12);
+text(axVec{4},0.05,0.9,'D','units','normalized','fontweight','bold','fontsize',12);
+printGraph('../graphs/Vthreshold_with_susceptibility');
